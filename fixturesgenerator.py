7e512b4771d87dd411fdfe2603e038355b91bea9
olejonny <olejonny.nyhus@gmail.com>
2024-04-29 13:01:18 +0200

    Second

--- fixturesgenerator.py
+++ fixturesgenerator.py
@@ -25,8 +25,9 @@ def excel_to_json(excel_file_path, model_name, json_file_path):
         json.dump(records, json_file, indent=4)
 
 
-def csv_to_json(csv_file_path, model_name, json_file_path):
+def csv_to_json(csv_file, model_name):
     # Read the csv file
+    csv_file_path = f"raw/{csv_file}.csv"
     df = pd.read_csv(csv_file_path)
 
     # Initialize an empty list to store the records
@@ -43,14 +44,23 @@ def csv_to_json(csv_file_path, model_name, json_file_path):
         # Append the dictionary to the list
         records.append(record)
 
+    json_file_path = f"fixtures/{csv_file}.json"
     # Write the list to a json file
     with open(json_file_path, "w") as json_file:
         json.dump(records, json_file, indent=4)
 
 
 if __name__ == "__main__":
-    my_file = "raw/component_k_value.csv"
-    output_file = "component_k_value.json"
-    model_name = "headloss.KValue"
+    my_files = {
+        "headloss.KValue": "component_k_value",
+        "headloss.PipeDimension": "pipe_dimension",
+        "headloss.PipeMaterial": "pipe_material",
+        "water.Dscw_1_9": "dscw_1_9",
+        "water.Dscw_1_10": "dscw_1_10",
+        "water.Dscw_1_11": "dscw_1_11",
+        "water.Dscw_1_12": "dscw_1_12",
+        "water.Dscw_1_13": "dscw_1_13",
+    }
 
-    csv_to_json(my_file, model_name, output_file)
+    for key, value in my_files.items():
+        csv_to_json(value, key)
